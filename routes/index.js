e85186006cc96ffcfc6b6dcb6f9854a4ae73c6fb
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-08-06 11:32:51 -0600

    changed hello world to look like json

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send("Hello world!");
+  res.status(200).json({ message: "Hello world!" });
 });
 
 module.exports = router;
